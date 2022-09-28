fda095053f22fe09754eab92607e51f9b82e1968
Nuel <villanueva.antony@gmail.com>
2022-09-28 10:07:45 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Tony - ISEC6000'));
+app.get('/', (req, res) => res.send('Module 5, Full pipeline test - Tony - ISEC6000, 28 Sept 22'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
